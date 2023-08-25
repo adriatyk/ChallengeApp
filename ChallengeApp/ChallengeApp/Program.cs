0471166ed3bb430e09203d77a8bf864d0303710c
adriatyk <adriatyk@gmail.com>
2023-08-25 22:42:07 +0200

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Adrian!");
+﻿var IMIE = "EWA";
+bool KOBIETA = true;
+int WIEK = 20;
+
+if (KOBIETA == true && WIEK < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Kobieta ma 30 lub więcej lat");
+}
+WIEK = WIEK + 13;
+if (IMIE == "EWA" && WIEK == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+KOBIETA = false;
+if (KOBIETA == false || WIEK < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+
+
