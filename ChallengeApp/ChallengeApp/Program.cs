6d91aa0de0830dce6a84327d88298c102267827b
adriatyk <adriatyk@gmail.com>
2023-09-02 10:41:35 +0200

    Zadanie domowe dzien 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,29 @@
-﻿var name = "Ewa";
-bool woman = false;
-int age = 16;
+﻿int number = 14374;
+string numberInString = number.ToString();
+char[] digitsNumber = numberInString.ToArray();
+char[] digits = new char[10];
+digits[0] = '0';
+digits[1] = '1';
+digits[2] = '2';
+digits[3] = '3';
+digits[4] = '4';
+digits[5] = '5';
+digits[6] = '6';
+digits[7] = '7';
+digits[8] = '8';
+digits[9] = '9';
 
-if (woman == true && age < 30)
+Console.WriteLine("Wyniki dla liczby: " + number);
+
+foreach (char digit1 in digits)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    int n = 0;
+    foreach (char digit2 in digitsNumber)
+    {
+        if (digit1 == digit2)
+        {
+            n++;
+        }
+    }
+    Console.WriteLine(digit1 + " => " + n);
 }
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (woman != true && age < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-
-
-
-
-
